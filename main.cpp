89a9cf955cf30768a4eaf128574b1fe04d2114c6
Dino-Zunic <168399562+Dino-Zunic@users.noreply.github.com>
2024-09-23 21:35:44 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@
 
 std::ofstream camera_position_file("camera_positions.txt");
 
-renderer rend;
-
 bool updated = true;
 
 enum class Mode {
@@ -19,9 +17,6 @@ enum class Mode {
 };
 
 Mode mode = Mode::EXPLORE;
-
-const int WIDTH = 800;
-const int HEIGHT = 600;
 
 BITMAPINFO bmi;
 HBITMAP hBitmap;
@@ -40,8 +35,8 @@ void initBitmap(HWND hwnd) {
     hMemDC = CreateCompatibleDC(GetDC(hwnd));
 
     bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-    bmi.bmiHeader.biWidth = WIDTH;
-    bmi.bmiHeader.biHeight = -HEIGHT; // Top-down DIB
+    bmi.bmiHeader.biWidth = CAMERA_WIDTH;
+    bmi.bmiHeader.biHeight = -CAMERA_HEIGHT; // Top-down DIB
     bmi.bmiHeader.biPlanes = 1;
     bmi.bmiHeader.biBitCount = 32;
     bmi.bmiHeader.biCompression = BI_RGB;
@@ -58,12 +53,12 @@ void initBitmap(HWND hwnd) {
 void render() {
     QueryPerformanceCounter(&renderStart);
 
-    renderer_render(&rend, get_scene());
+    renderer_render();
 
-    for (int y = 0; y < HEIGHT; ++y) {
-        for (int x = 0; x < WIDTH; ++x) {
-            vec3 color = renderer_get_color(&rend, HEIGHT - 1 - y, x);
-            pBitmapData[y * WIDTH + x] = RGB(float_real(color.x) * 255, float_real(color.y) * 255, float_real(color.z) * 255);
+    for (int y = 0; y < CAMERA_HEIGHT; ++y) {
+        for (int x = 0; x < CAMERA_WIDTH; ++x) {
+            vec3 color = renderer_canvas[CAMERA_HEIGHT - 1 - y][x];
+            pBitmapData[y * CAMERA_WIDTH + x] = RGB(color.z >> 8, color.y >> 8, color.x >> 8);
         }
     }
 
@@ -72,16 +67,21 @@ void render() {
 }
 
 void snapPicture(HWND hwnd) {
-    camera *cam = scene_get_camera(get_scene());
     mode = Mode::PICTURE;
-    for (int y = 0; y < HEIGHT; ++y) {
-        for (int x = 0; x < WIDTH; ++x) {
-            real u = real_int(x - 400) >> 9;
-            real v = real_int(HEIGHT - 1 - y - 300) >> 9;
-            vec3 temp0 = camera_get_position(cam), temp1 = camera_get_ray_direction(cam, u, v);
-            ray ray = ray_init(&temp0, &temp1);
-            vec3 color = scene_trace_ray(get_scene(), &ray, 0);
-            pBitmapData[y * WIDTH + x] = RGB(float_real(color.x) * 255, float_real(color.y) * 255, float_real(color.z) * 255);
+    for (int y = 0; y < CAMERA_HEIGHT; ++y) {
+        for (int x = 0; x < CAMERA_WIDTH; ++x) {
+            real u = (x - 400) << 7;
+            real v = (CAMERA_HEIGHT - 1 - y - 300) << 7;
+            vec3 temp = camera_get_ray_direction(u, v);
+            ray ray;
+            ray.origin.x = camera_position->x;
+            ray.origin.y = camera_position->y;
+            ray.origin.z = camera_position->z;
+            ray.direction.x = temp.x;
+            ray.direction.y = temp.y;
+            ray.direction.z = temp.z;
+            vec3 color = scene_trace_ray(&ray, 0);
+            pBitmapData[y * CAMERA_WIDTH + x] = RGB(color.z >> 8, color.y >> 8, color.x >> 8);
         }
     }
     InvalidateRect(hwnd, NULL, FALSE);
@@ -92,86 +92,76 @@ void display(HWND hwnd) {
     QueryPerformanceCounter(&displayStart);
 
     HDC hdc = GetDC(hwnd);
-    BitBlt(hdc, 0, 0, WIDTH, HEIGHT, hMemDC, 0, 0, SRCCOPY);
+    BitBlt(hdc, 0, 0, CAMERA_WIDTH, CAMERA_HEIGHT, hMemDC, 0, 0, SRCCOPY);
     ReleaseDC(hwnd, hdc);
 
     QueryPerformanceCounter(&displayEnd);
     displayTime = double(displayEnd.QuadPart - displayStart.QuadPart) / frequency.QuadPart;
 }
 
-real SPEED_YAW = real_float(0.6f);
-real SPEED_PITCH = real_float(0.6f);
+real SPEED_YAW = 0x00008000;
+real SPEED_PITCH = 0x00008000;
 real SPEED_MOVE = real_mul(REAL_PI, SPEED_YAW);
 
 real speed_move, speed_yaw, speed_pitch;
 int tab_pressed = 0;
 int esc_pressed = 0;
 
-bool is_sphere_creating = false;
-real sphere_radius = real_float(1.0f);
-vec3 sphere_position;
-
 POINT last_mouse_pos;
 bool right_mouse_pressed = false;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    scene *sc = get_scene();
     switch (uMsg) {
     case WM_MOUSEMOVE:
     {
-        if (mode == Mode::EXPLORE) {
-            // Handle mouse movement to adjust camera
-            POINT currentMousePos;
-            GetCursorPos(&currentMousePos);
-
-            real deltaX = (currentMousePos.x - windowCenter.x) << 7;
-            real deltaY = (currentMousePos.y - windowCenter.y) << 7;
-
-            camera *cam = scene_get_camera(get_scene());
-
-            real yaw_change = -real_mul(deltaX, SPEED_YAW);
-            real pitch_change = -real_mul(deltaY, SPEED_PITCH);
-
-            real yaw = camera_get_yaw(cam);
-            real pitch = camera_get_pitch(cam);
-
-            yaw = real_add(yaw, yaw_change);
-            pitch = real_add(pitch, pitch_change);
-
-            if (pitch > REAL_PI_TWO) pitch = REAL_PI_TWO;
-            if (pitch < -REAL_PI_TWO) pitch = REAL_PI_TWO;
-
-            camera_set_yaw(cam, yaw);
-            camera_set_pitch(cam, pitch);
-            camera_update_angles(cam);
-
-            last_mouse_pos = currentMousePos;
-
-            SetCursorPos(windowCenter.x, windowCenter.y);
-
-            updated = true;
+        if (mode != Mode::EXPLORE) {
+            break;
         }
+        // Handle mouse movement to adjust camera
+        POINT currentMousePos;
+        GetCursorPos(&currentMousePos);
+
+        real deltaX = (currentMousePos.x - windowCenter.x) << 7;
+        real deltaY = (currentMousePos.y - windowCenter.y) << 7;
+
+        real yaw_change = -real_mul(deltaX, SPEED_YAW);
+        real pitch_change = -real_mul(deltaY, SPEED_PITCH);
+
+        *camera_yaw += yaw_change;
+        *camera_pitch += pitch_change;
+
+        if (*camera_yaw < -REAL_PI_TWO) *camera_yaw = REAL_PI_TWO;
+        if (*camera_yaw > REAL_PI_TWO) *camera_yaw = -REAL_PI_TWO;
+        if (*camera_pitch < -REAL_PI_TWO) *camera_pitch = +REAL_PI_TWO;
+        if (*camera_pitch > REAL_PI_TWO) *camera_pitch = -REAL_PI_TWO;
+
+        camera_update();
+
+        last_mouse_pos = currentMousePos;
+
+        SetCursorPos(windowCenter.x, windowCenter.y);
+
+        updated = true;
         break;
     }
     case WM_RBUTTONUP:
     {
-        if (mode == Mode::EXPLORE) {
-            if (!sc->ghost_mode) {
-                right_mouse_pressed = true;
-                sphere_position = camera_get_forward(&sc->cam);
-                sc->ghost_mode = true;
-                sc->ghost_radius = 0x00010000;
-                updated = true;
-            }
-            else {
-                sc->ghost_mode = false;
-                vec3 c = camera_get_position(&sc->cam), forward = camera_get_forward(&sc->cam);
-                forward = vec3_multiply_scalar(&forward, SCENE_SPAWN_DISTANCE + sc->ghost_radius);
-                c = vec3_add(&c, &forward);
-                material m = { material_color_rgb(0xFFFF00), real_float(0.5f) };
-                sphere new_sphere = sphere_init_with_values(c, sc->ghost_radius, m);
-                scene_add_sphere(sc, &c, sc->ghost_radius);
-            }
+        if (mode != Mode::EXPLORE) {
+            break;
+        }
+        if (!*scene_ghost_mode) {
+            right_mouse_pressed = true;
+            *scene_ghost_mode = true;
+            *scene_ghost_radius = SCENE_GHOST_RADIUS_INIT;
+            *scene_ghost_radius2 = SCENE_GHOST_RADIUS_INIT2;
+            updated = true;
+        }
+        else {
+            *scene_ghost_mode = false;
+            vec3 c = *camera_position, forward = *camera_forward;
+            forward = vec3_multiply_scalar(&forward, SCENE_SPAWN_DISTANCE + *scene_ghost_radius);
+            c = vec3_add(&c, &forward);
+            scene_add_sphere(&c, *scene_ghost_radius);
         }
         break;
     }
@@ -181,34 +171,40 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
         real min_t = 0x7fffffff;
         sphere *min_s = nullptr;
-        for (int i = 0; i < sc->num_of_spheres; i++) {
+        for (int i = 0; i < *scene_num_of_spheres; i++) {
             real t;
-            vec3 forward = camera_get_forward(&sc->cam);
-            ray r = ray_init(&sc->cam.position, &forward);
-            if (sphere_intersect(&sc->spheres[i], &r, &t)) {
+            ray r;
+            r.origin.x = camera_position->x;
+            r.origin.y = camera_position->y;
+            r.origin.z = camera_position->z;
+            r.direction.x = camera_forward->x;
+            r.direction.y = camera_forward->y;
+            r.direction.z = camera_forward->z;
+            if (sphere_intersect(&scene_spheres[i], &r, &t)) {
                 if (t < min_t) {
                     min_t = t;
-                    min_s = &sc->spheres[i];
+                    min_s = &scene_spheres[i];
                 }
             }
         }
-        sc->marked = min_s;
+        *scene_marked_sphere_address = min_s;
         updated = true;
         break;
     }
     case WM_MOUSEWHEEL:
     {
-        if (sc->ghost_mode) {
+        if (*scene_ghost_mode) {
             int mouse_delta = GET_WHEEL_DELTA_WPARAM(wParam);
-            const real radius_delta = real_float(0.03f);
-            const real radius_min = real_float(0.3f);
+            const real radius_delta = 0x00010000 >> 5;
             if (mouse_delta > 0) {
-                sc->ghost_radius = real_add(sc->ghost_radius, radius_delta);
+                *scene_ghost_radius += radius_delta;
+                *scene_ghost_radius2 = real_mul(*scene_ghost_radius, *scene_ghost_radius);
             }
             else {
-                sc->ghost_radius = real_sub(sc->ghost_radius, radius_delta);
-                if (sc->ghost_radius < radius_min) {
-                    sc->ghost_radius = radius_min; // Minimum radius
+                *scene_ghost_radius -= radius_delta;
+                if (*scene_ghost_radius < SCENE_GHOST_RADIUS_MIN) {
+                    *scene_ghost_radius = SCENE_GHOST_RADIUS_MIN;
+                    *scene_ghost_radius2 = SCENE_GHOST_RADIUS_MIN2;
                 }
             }
             updated = true;
@@ -239,58 +235,47 @@ void exploreMode(HWND hwnd, real delta_time) {
     speed_yaw -= real_mul(delta_time, SPEED_YAW) >> 1;
     speed_pitch -= real_mul(delta_time, SPEED_PITCH) >> 1;
 
-    scene *sc = get_scene();
-    camera *cam = scene_get_camera(sc);
-    vec3 pos = camera_get_position(cam);
-    real yaw = camera_get_yaw(cam);
-    real pitch = camera_get_pitch(cam);
     vec3 temp_vec3; // helper for vec3 operations
     real temp_real; // helper for real operations
 
     if (GetAsyncKeyState('W') & 0x8000) {
-        temp_vec3 = camera_get_forward(cam);
         temp_real = real_mul(SPEED_MOVE, delta_time);
-        temp_vec3 = vec3_multiply_scalar(&temp_vec3, temp_real);
-        pos = vec3_add(&pos, &temp_vec3);
+        temp_vec3 = vec3_multiply_scalar(camera_forward, temp_real);
+        *camera_position = vec3_add(camera_position, &temp_vec3);
         updated = true;
     }
     if (GetAsyncKeyState('S') & 0x8000) {
-        temp_vec3 = camera_get_forward(cam);
         temp_real = real_mul(SPEED_MOVE, delta_time);
-        temp_vec3 = vec3_multiply_scalar(&temp_vec3, temp_real);
-        pos = vec3_subtract(&pos, &temp_vec3);
+        temp_vec3 = vec3_multiply_scalar(camera_forward, temp_real);
+        *camera_position = vec3_subtract(camera_position, &temp_vec3);
         updated = true;
     }
     if (GetAsyncKeyState('A') & 0x8000) {
-        temp_vec3 = camera_get_right(cam);
         temp_real = real_mul(SPEED_MOVE, delta_time);
-        temp_vec3 = vec3_multiply_scalar(&temp_vec3, temp_real);
-        pos = vec3_subtract(&pos, &temp_vec3);
+        temp_vec3 = vec3_multiply_scalar(camera_right, temp_real);
+        *camera_position = vec3_subtract(camera_position, &temp_vec3);
         updated = true;
     }
     if (GetAsyncKeyState('D') & 0x8000) {
-        temp_vec3 = camera_get_right(cam);
         temp_real = real_mul(SPEED_MOVE, delta_time);
-        temp_vec3 = vec3_multiply_scalar(&temp_vec3, temp_real);
-        pos = vec3_add(&pos, &temp_vec3);
+        temp_vec3 = vec3_multiply_scalar(camera_right, temp_real);
+        *camera_position = vec3_add(camera_position, &temp_vec3);
         updated = true;
     }
     if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
-        temp_real = real_mul(SPEED_MOVE, delta_time);
-        pos.y = real_add(pos.y, temp_real);
+        camera_position->y += real_mul(SPEED_MOVE, delta_time);
         updated = true;
     }
     if (GetAsyncKeyState(VK_SHIFT) & 0x8000) {
-        temp_real = real_mul(SPEED_MOVE, delta_time);
-        pos.y = real_sub(pos.y, temp_real);
-        if (pos.y < real_float(0.1f)) {
-            pos.y = real_float(0.1f);
+        camera_position->y -= real_mul(SPEED_MOVE, delta_time);
+        if (camera_position->y < 0x00001000) {
+            camera_position->y = 0x00001000;
         }
         updated = true;
     }
     if (GetAsyncKeyState(VK_DELETE) & 0x8000) {
-        scene_remove_sphere(sc, sc->marked);
-        sc->marked = nullptr;
+        scene_remove_sphere(*scene_marked_sphere_address);
+        *scene_marked_sphere_address = nullptr;
         updated = true;
     }
     if (GetAsyncKeyState(VK_RETURN) & 0x8000) {
@@ -318,32 +303,7 @@ void exploreMode(HWND hwnd, real delta_time) {
         tab_pressed = 0;
     }
 
-    camera_set_position(cam, &pos);
-
-    if (is_sphere_creating) {
-        scene_add_sphere(get_scene(), &sphere_position, sphere_radius);
-        is_sphere_creating = false;
-        updated = true;
-    }
-
     if (updated) {
-        if (yaw < 0) {
-            yaw += REAL_PI_TWO;
-        }
-        if (yaw >= REAL_PI_TWO) {
-            yaw -= REAL_PI_TWO;
-        }
-        if (pitch < 0) {
-            pitch += REAL_PI_TWO;
-        }
-        if (pitch >= REAL_PI_TWO) {
-            pitch -= REAL_PI_TWO;
-        }
-        camera_set_position(cam, &pos);
-        camera_set_yaw(cam, yaw);
-        camera_set_pitch(cam, pitch);
-        camera_update_angles(cam);
-
         render();
         InvalidateRect(hwnd, NULL, FALSE);
         UpdateWindow(hwnd);
@@ -366,25 +326,8 @@ void pictureMode(HWND hwnd, real delta_time) {
     }
 }
 
-void initColors() {
-    material_light_mode = 0;
-
-    material_color_sky[0] = material_color_rgb(0x87CEFB);  // Light blue for normal mode
-    material_color_sky[1] = material_color_rgb(0x1f009F);  // Neon magenta (purple) for retro mode
-
-    material_color_horizon[0] = material_color_rgb(0xFFFF00);  // Orange-red for normal sunset horizon
-    material_color_horizon[1] = material_color_rgb(0xFF00FF);  // Bright yellow for retro mode
-
-    material_color_floor1[0] = material_color_rgb(0xB0E0E6);  // Light foggy blue for normal mode (for a sky-like ground effect)
-    material_color_floor1[1] = material_color_rgb(0x00FFFF);  // Bright cyan for retro mode
-
-    material_color_floor2[0] = material_color_rgb(0x2F4F6F);  // Dark gray-blue for normal mode (to mimic sky reflection)
-    material_color_floor2[1] = material_color_rgb(0x8A2BE2);  // Neon purple for retro mode
-
-}
-
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    initColors();
+    material_init_colors();
 
     const wchar_t CLASS_NAME[] = L"RayTracingDemo";
     WNDCLASS wc = {};
@@ -400,7 +343,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         CLASS_NAME,
         L"Ray Tracing Demo",
         WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, WIDTH, HEIGHT,
+        CW_USEDEFAULT, CW_USEDEFAULT, CAMERA_WIDTH, CAMERA_HEIGHT,
         NULL,
         NULL,
         hInstance,
@@ -416,7 +359,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     initBitmap(hwnd);
 
-    scene_reset(get_scene());
+    scene_reset();
 
     QueryPerformanceFrequency(&frequency);
 
@@ -442,7 +385,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         }
 
         QueryPerformanceCounter(&frameEnd);
-        real delta_time = real_float(double(frameEnd.QuadPart - frameStart.QuadPart) / frequency.QuadPart);
+        uint32_t highFrequency = (1 << 16) * double(frameEnd.QuadPart - frameStart.QuadPart) / frequency.QuadPart;
+        real delta_time = highFrequency;
         frameStart = frameEnd;
 
         switch (mode) {
